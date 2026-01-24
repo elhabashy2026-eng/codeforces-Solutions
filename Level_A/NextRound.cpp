632945c69b99cf06307e52434f45632f10ab9d11
elhabashy2026-eng <elhabashy2026@gmail.com>
2026-01-24 20:57:00 +0200

    Refactor NextRound.cpp for improved readability

--- Level_A/NextRound.cpp
+++ Level_A/NextRound.cpp
@@ -7,20 +7,26 @@ int main()
    int Stantard;
    int NumberAllowed[55];
    int NumberOfPassed = 0;
-   
+
    cin >> NumberOfParticipants;
    cin >> Stantard;
+   if (Stantard <= NumberOfParticipants)
+   {
+      for (int i = 0; i < NumberOfParticipants; i++)
+      {
+         cin >> NumberAllowed[i];
+      }
 
-     for(int i = 0 ; i < NumberOfParticipants ; i++){
-        cin >> NumberAllowed[i];
-     }
-        
-     for(int j = 0 ; j < NumberOfParticipants ; j++){
-       if(NumberAllowed[j] >= NumberAllowed[Stantard - 1] && NumberAllowed[j] > 0){
-         NumberOfPassed += 1;
-       }
-     }
-     cout << NumberOfPassed;
-
+      for (int j = 0; j < NumberOfParticipants; j++)
+      {
+         if (NumberAllowed[j] >= NumberAllowed[Stantard - 1] && NumberAllowed[j] > 0)
+         {
+            NumberOfPassed += 1;
+         }
+      }
+      cout << NumberOfPassed;
+   }
+   else
+      cout << "Error: k cannot be larger than n!";
    return 0;
 }
