48ec135c4a7d15c4638ba405498e7c5acecc9633
elhabashy2026-eng <elhabashy2026@gmail.com>
2026-03-15 17:17:27 +0200

    Refactor watermelon weight check logic

--- Level_A/Watermelon.cpp
+++ Level_A/Watermelon.cpp
@@ -1,21 +1,20 @@
 #include <iostream>
+
 using namespace std;
 
-int main()
-{
-   int weight;
-   
-   cout << "Enter the weight of the watermelon: ";
-   cin >> weight;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-   if (weight > 2 && weight % 2 == 0)
-   {
-      cout << "YES";
-   }
-      else
-      {
-         cout << "NO";
-      }
+    int watermelonWeight;
+    
+    if (!(cin >> watermelonWeight)) return 0;
 
-   return 0;
+    if (watermelonWeight > 2 && watermelonWeight % 2 == 0) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+
+    return 0;
 }
