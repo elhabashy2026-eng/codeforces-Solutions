1b7c755755c866205751349979c5b62ad1f4e2bb
elhabashy2026-eng <elhabashy2026@gmail.com>
2026-03-15 17:13:48 +0200

    Refactor PetyaAndStrings for better performance
    Refactor string comparison to use transform for case insensitivity and improve I/O performance.

--- Level_A/PetyaAndStrings.cpp
+++ Level_A/PetyaAndStrings.cpp
@@ -1,36 +1,26 @@
 #include <iostream>
+#include <string>
+#include <algorithm>
+
 using namespace std;
 
-int main()
-{
-   string word1, word2;
-   cin >> word1;
-   cin >> word2;
-   for (int i = 0; i < word1.length(); i++)
-   {
-      word1[i] = tolower(word1[i]);
-   }
-   for (int i = 0; i < word2.length(); i++)
-   {
-      word2[i] = tolower(word2[i]);
-   }
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-   for (int i = 0; i < word1.length(); i++)
-   {
-      if (word1[i] == word2[i])
-         continue;
-      else if (word1[i] > word2[i])
-      {
-         cout << 1;
-         return 0;
-      }
-      else
-      {
-         cout << -1;
-         return 0;
-      }
-   }
-   cout << 0;
+    string firstWord, secondWord;
+    cin >> firstWord >> secondWord;
 
-   return 0;
+    transform(firstWord.begin(), firstWord.end(), firstWord.begin(), ::tolower);
+    transform(secondWord.begin(), secondWord.end(), secondWord.begin(), ::tolower);
+
+    if (firstWord < secondWord) {
+        cout << -1 << "\n";
+    } else if (firstWord > secondWord) {
+        cout << 1 << "\n";
+    } else {
+        cout << 0 << "\n";
+    }
+
+    return 0;
 }
