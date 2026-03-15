b0ea7448d119b3ac013097f00a79443d007cb51a
elhabashy2026-eng <elhabashy2026@gmail.com>
2026-03-15 17:14:46 +0200

    Refactor problem counting logic in Team.cpp

--- Level_A/Team.cpp
+++ Level_A/Team.cpp
@@ -1,31 +1,31 @@
 #include <iostream>
+
 using namespace std;
 
-int main()
-{
-   int NumOfProblem_T = 0;
-   int NumberOfProblems;
-   bool state;
-   cin >> NumberOfProblems;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-   for (int i = 0; i < NumberOfProblems; i++)
-   {
-      int Num_T = 0;
-      for (int j = 0; j < 3; j++)
-      {
-         cin >> state;
-         if (state)
-         {
-            Num_T += 1;
-         }
-      }
-      if (Num_T >= 2)
-      {
-         NumOfProblem_T += 1;
-      }
-   }
+    int numberOfProblems;
+    if (!(cin >> numberOfProblems)) return 0;
 
-   cout << NumOfProblem_T;
+    int totalSolvedProblems = 0;
 
-   return 0;
+    for (int i = 0; i < numberOfProblems; i++) {
+        int friendsAgreementCount = 0;
+        int currentFriendDecision;
+
+        for (int j = 0; j < 3; j++) {
+            cin >> currentFriendDecision;
+            friendsAgreementCount += currentFriendDecision;
+        }
+
+        if (friendsAgreementCount >= 2) {
+            totalSolvedProblems++;
+        }
+    }
+
+    cout << totalSolvedProblems << "\n";
+
+    return 0;
 }
